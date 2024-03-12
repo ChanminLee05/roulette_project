991c948763457fc59fb18c894a2e5f86fb82b2e8
Chanmin Lee <lee00837@algonquinlive.com>
2024-03-11 19:46:03 -0600

    final

--- src/Components/Meat-based-food.js
+++ src/Components/Meat-based-food.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import './SpinPage.css';
 import {Link} from "react-router-dom";
 import backArrowImg from "../Assets/back-arrow.png";
+import SideBar from "./SideBar";
 
 export default function Rice() {
     const [rotation, setRotation] = useState(0);
@@ -18,19 +19,24 @@ export default function Rice() {
     return(
         <div className={isFoodBackground ? "food-background" : "main-background"}>
             <div className="header-container">
+                <div className="roulette-container">
+                    <h1 className="roulette-title">What To Eat Roulette Game</h1>
+                    <p className="roulette-desc">The roulette game is a fun way to decide what to eat when you're feeling indecisive.
+                        Simply select the type of food you're in the mood for, spin the roulette wheel,
+                        and let it randomly choose your meal for you.
+                        Once you've spun the wheel and settled on a meal choice, you can easily find recipes to prepare your selected dish.
+                    </p>
+                </div>
                 <div className="img-container" onClick={changeBackgroundImg}>
                     <Link to="/main">
                         <img className="back-img" src={backArrowImg} alt={backArrowImg}/>
                     </Link>
                 </div>
-                <div className="roulette-container">
-                    <h1 className="roulette-title">What To Eat Roulette Game</h1>
-                </div>
             </div>
             <div className="wheel-container">
                 <button className="spinButton" onClick={spinRoulette}>spin</button>
                 <div className="wheel" style={{ transform: `rotate(${rotation}deg)`}}>
-                    <div className="roulette-item" style={{ background: '#db7093', transform: 'rotate(30deg)' }}><span>카레</span></div>
+                    <div className="roulette-item" style={{ background: '#db7093', transform: 'rotate(30deg)' }}><span>Curry</span></div>
                     <div className="roulette-item" style={{ background: '#20b2aa', transform: 'rotate(60deg)' }}><span>닭 볶음탕</span></div>
                     <div className="roulette-item" style={{ background: '#d63e92', transform: 'rotate(90deg)' }}><span>덮밥</span></div>
                     <div className="roulette-item" style={{ background: '#daa520', transform: 'rotate(120deg)' }}><span>된장 찌개</span></div>
@@ -44,6 +50,7 @@ export default function Rice() {
                     <div className="roulette-item" style={{ background: '#4374af', transform: 'rotate(360deg)' }}><span>콩나물 국</span></div>
                 </div>
             </div>
+            <SideBar/>
         </div>
     )
 }
